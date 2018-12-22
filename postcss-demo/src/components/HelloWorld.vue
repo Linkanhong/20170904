816d1915d39fd72db619cce97db896c748cbba78
林龛洪 <31609687+Linkanhong@users.noreply.github.com>
2018-12-23 01:23:55 +0800

    Add files via upload

---
+++ postcss-demo/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div class="box">1313</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.box{
+  width: 750px;
+  height: 100px;
+  background-color: #f60;
+  box-shadow: 1px 2px 5px 9px red;
+}
+</style>
